97647834ebcfbca24ce79b19d48be65fb5d9b663
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-24 11:36:05 -0500

    Importacion del modulo de mongose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), AuthModule],
   controllers: [],
   providers: [],
 })
