b53fb4a0fbaf17243cf488950aaa9bb3caeccfe7
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-24 12:19:50 -0500

    Se requiere de instalacion del dotenv y su require

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+const db = process.env.MONGO_URI;
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost:27017'), AuthModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(db);
+  }
+}
