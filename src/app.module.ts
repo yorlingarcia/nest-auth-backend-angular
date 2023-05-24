f2292fe949ff038e0f04066a3727b6902bd674c5
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-24 11:37:16 -0500

    Actualizacion de la ruta

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), AuthModule],
   controllers: [],
   providers: [],
 })
