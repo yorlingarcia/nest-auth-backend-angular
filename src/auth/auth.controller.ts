23abe3896a8e74c5c1ffd56cb149d1fe5c4400ed
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-05-30 17:23:26 -0500

    creacion de una validacion del jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,6 +13,9 @@ import {
 import { AuthService } from './auth.service';
 import { CreateUserDto, UpdateAuthDto, LoginDto, RegisterUserDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
+import { get } from 'http';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -38,6 +41,17 @@ export class AuthController {
     //const user = req['user'];
     //return user;
     return this.authService.findAll();
+  }
+
+  //loginResponse
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken(@Request() req: Request): LoginResponse {
+    const user = req['user'] as User;
+    return {
+      user,
+      token: this.authService.getJwtToken({ id: user._id }),
+    };
   }
 
   @Get(':id')
